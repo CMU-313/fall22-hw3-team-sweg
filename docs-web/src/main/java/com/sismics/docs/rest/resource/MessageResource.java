121e904e5662ee2408a322d601abc7ce9d096b4f
vwaddell <98916181+vwaddell@users.noreply.github.com>
2022-10-06 22:24:59 -0400

    Create inbox messages (#37)

--- docs-web/src/main/java/com/sismics/docs/rest/resource/MessageResource.java
+++ docs-web/src/main/java/com/sismics/docs/rest/resource/MessageResource.java
@@ -60,7 +60,7 @@ public class MessageResource extends BaseResource {
         JsonArrayBuilder messages = Json.createArrayBuilder();
         if (sortColumn == null) {
             // Sort by timestamp by default
-            sortColumn = 4;
+            sortColumn = 5;
             asc = false;
         }
         SortCriteria sortCriteria = new SortCriteria(sortColumn, asc);
@@ -68,7 +68,7 @@ public class MessageResource extends BaseResource {
 
         MessageDao messageDao = new MessageDao();
         List<MessageDto> messageDtoList = messageDao.findByCriteria(principal.getId(), messageCriteria, sortCriteria);
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd h:mm:ss a");
         for (MessageDto messageDto : messageDtoList) {
             messages.add(Json.createObjectBuilder()
                     .add("id", messageDto.getId())
--- docs-web/src/main/webapp/src/app/docs/controller/inbox/Inbox.js
+++ docs-web/src/main/webapp/src/app/docs/controller/inbox/Inbox.js
@@ -4,4 +4,14 @@
  * Inbox controller.
  */
 angular.module('docs').controller('Inbox', function($scope, Restangular) {
+
+    // get list of user's messages from server
+    $scope.loadMessages = function() {
+        Restangular.one('messages').get().then(function(data) {
+            $scope.messages = data.messages;
+        });
+    };
+
+    $scope.loadMessages();
+
 });
