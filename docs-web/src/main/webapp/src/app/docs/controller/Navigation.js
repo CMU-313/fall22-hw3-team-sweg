9a43236c83cc4976e31ed365001f38bf45db8590
Kyungmin Kim <kyminbb.kim@gmail.com>
2022-10-06 23:49:56 -0400

    Fix `navigation.js`

--- docs-web/src/main/webapp/src/app/docs/controller/Navigation.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Navigation.js
@@ -3,11 +3,11 @@
 /**
  * Navigation controller.
  */
-angular.module('docs').controller('Navigation', function($scope, $state, $stateParams, $rootScope, User, Restangular) {
-  User.userInfo().then(function(data) {
+angular.module('docs').controller('Navigation', function ($scope, $state, $stateParams, $rootScope, User, Restangular) {
+  User.userInfo().then(function (data) {
     $rootScope.userInfo = data;
     if (data.anonymous) {
-      if($state.current.name !== 'login') {
+      if ($state.current.name !== 'login') {
         $state.go('login', {
           redirectState: $state.current.name,
           redirectParams: JSON.stringify($stateParams),
@@ -20,7 +20,7 @@ angular.module('docs').controller('Navigation', function($scope, $state, $stateP
 
   $scope.unreadCount = 0;
   async function getUnreadMessageCount() {
-    let response = await Restangular.one("messages", "unread_count").get({count: $scope.unreadCount});
+    let response = await Restangular.one("messages", "unread_count").get({ count: $scope.unreadCount });
     $scope.unreadCount = response.count;
     await getUnreadMessageCount();
   }
@@ -30,9 +30,9 @@ angular.module('docs').controller('Navigation', function($scope, $state, $stateP
   /**
    * User logout.
    */
-  $scope.logout = function($event) {
-    User.logout().then(function() {
-      User.userInfo(true).then(function(data) {
+  $scope.logout = function ($event) {
+    User.logout().then(function () {
+      User.userInfo(true).then(function (data) {
         $rootScope.userInfo = data;
       });
       $state.go('main');
